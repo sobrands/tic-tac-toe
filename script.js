c0d5c4c2ccc8bdf5f38474f079f0d77e84455a73
brandon <lim.wx.brandon@gmail.com>
2023-10-18 18:27:39 +0800

    Added input functionality

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+function createPlayer(playerName, playerSymbol) {
+  const name = playerName;
+  const symbol = playerSymbol;
+
+  return {
+    name, 
+    symbol
+  };
+}
+
 const gameBoard = (function() {
   let board = [...Array(3)].map(e => Array(3));
 
   function getBoard() { return board; };
 
+  function editBoard(grid, symbol) {
+    const row = grid.dataset.row;
+    const col = grid.dataset.col;
+    board[row][col] = symbol;
+  }
+
   return {
     getBoard,
+    editBoard,
   }
 })();
 
 const displayController = (function () {
   
+  const startGameBtn = document.querySelector(".start-game");
+  const playerTurn = document.querySelector(".player-turn");
+  const gameArena = document.querySelector(".game-board");
+  
+  let currentPlayer;
+
   function displayBoard() {
     const ttcBoard = document.querySelector(".game-board");
     const currentBoard = gameBoard.getBoard();
@@ -27,19 +50,58 @@ const displayController = (function () {
     }
   }
 
-  return {
-    displayBoard,
-  };
-})();
+  function displayPlayerTurn() {
+    currentPlayer = gameController.getCurrentPlayer();
+    playerTurn.textContent = `${currentPlayer.name}'s Turn Now`;
+  }
 
-const gameController = (function () {
+  function registerInput(event) {
+    gameBoard.editBoard(event.target, currentPlayer.symbol);
+    event.target.textContent = currentPlayer.symbol;
+    gameController.changePlayer();
+    displayPlayerTurn();
+  }
+
+  function beginGame() {
+    displayPlayerTurn();
+    gameArena.addEventListener('click', (e) => {
+      if (e.target.matches('button')) {
+        registerInput(e);
+      }
+    })
+  }
 
   function initGame() {
-    displayController.displayBoard();
+    displayBoard();
+    startGameBtn.addEventListener('click', beginGame);
   }
 
   return {
     initGame,
+  };
+})();
+
+const gameController = (function () {
+  const player1 = createPlayer("Player1", "X");
+  const player2 = createPlayer("Player2", "O");
+  let activePlayer = player1;
+
+  function initGame() {
+    displayController.initGame();
+  }
+
+  function getCurrentPlayer() {
+    return activePlayer;
+  }
+
+  function changePlayer() {
+    activePlayer = (activePlayer === player1) ? player2 : player1;
+  }
+
+  return {
+    initGame,
+    getCurrentPlayer,
+    changePlayer
   };
 })();
 
